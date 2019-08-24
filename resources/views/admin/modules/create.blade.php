0cf246c1937c50126cf07be52acbd65a7b909fcf
Nichop96 <48830373+Nichop96@users.noreply.github.com>
2019-08-24 15:14:07 +0200

    bella

--- resources/views/admin/modules/create.blade.php
+++ resources/views/admin/modules/create.blade.php
@@ -86,7 +86,42 @@ Create module
                                     <button class="btn btn-primary repeater-add-btn">
                                         Add question
                                     </button>
-
+                                    <button class="btn btn-primary" data-toggle="modal" data-target="#selectModule" id="fetchall">
+                                        Import questions
+                                    </button>
+                                    <div class="modal fade" id="selectModule" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                          <div class="modal-content">
+                                            <div class="modal-header">
+                                              <h5 class="modal-title">Import questions</h5>
+                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                              </button>
+                                            </div>                                            
+                                            <div class="modal-body">
+                                                <h6>Select a module of wich you want to take some questions</h6>                                                                                                            
+                                                <div class="table-responsive">
+                                                    <table id="recent_surveys" class="table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Name</th>
+                                                                <th>Description</th>
+                                                                <th>Category</th>                        
+                                                                <th>Actions</th>                       
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody id="tbody">                                                                
+                                                        </tbody>
+                                                    </table>
+                                                </div>                                                        
+                                            </div>
+                                            <div class="modal-footer">
+                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                              <button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#importQuestions" id='module_selected'>Select module</button>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
 
                                 </div>
                                 <br>
@@ -95,8 +130,7 @@ Create module
                                 <div class="items" data-group="test">
                                     <br>
                                     <div class="card border-primary mb-3">
-                                        <!-- Repeater Content -->
-                                        <!--<h5 class="align-items-center"> Insert question </h5>-->
+                                       
                                         <div class="item-content card-body block align-self-lg-auto">
 
                                             <div class="form-group">
@@ -110,9 +144,7 @@ Create module
                                                 <div class="col">
                                                     <h5 for="leftlabel" class="col-lg-6 control-label">Left label</h5>
                                                     <input type="text" id="leftlabel" name="leftlabel" class="form-control leftlabel" placeholder="Low">
-                                                    <span class="invalid-feedback leftlabel-alert" hidden role="alert">
-                                                        <strong>Invalid filled!</strong>
-                                                    </span>
+                                                    
                                                 </div>
                                                 <div class="col">
                                                     <h5 for="rightlabel"  class="col-lg-6 control-label">Right label</h5>
@@ -188,6 +220,46 @@ Create module
         </div>                
     </div>
 </div>
+
+<div class="modal fade" id="importQuestions" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Import questions</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+                <div class="modal-body">
+                    <h6>Select the questions</h6>                                                                                
+                    <div class="table-responsive">
+                        <table id="recent_surveys" class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>                                                                        
+                                    <th>Label left</th>
+                                    <th>Label Right</th>
+                                    <th>Max rate</th>
+                                    <th>Actions</th> 
+                                </tr>
+                            </thead>
+                            <tbody id="tbodyquestions">                                                                
+                            </tbody>
+                        </table>
+                    </div>                          
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary" data-dismiss="modal" id='insertQuestions'>Import questions</button>
+                </div>
+
+          </div>
+        </div>
+    </div>
+</div> 
+
+
 <script type="text/javascript" src="{{ URL::asset('js/repeater.js') }}"></script>
 <script>
                         /* Create Repeater */
@@ -325,5 +397,190 @@ Create module
         }
     }
 </script>
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+ <script>
+    $(document).ready(function(){
+        
+        $('#fetchall').click(function(){
+	 fetchModules();         
+       });
+       
+        $('#module_selected').click(function(){
+	 fetchQuestions();         
+       });
 
+    function fetchModules(){
+        
+      $.ajax({
+        url: '{{route('admin.modules.getmodules')}}',
+        headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+        type: 'POST',
+        dataType: 'json',
+        success: function(response){ 
+          
+        $("#tbody").empty();
+          var len = 0;          
+          if(response[0].name != null){
+            len = 1;
+          }         
+          if(len > 0){
+            i = 0;
+            while(response[i].name != null)
+            {
+              var description = response[i].description;
+              var name = response[i].name; 
+              var category = response[i].category;
+              var id = response[i].id;
+
+              var tr_str = "<tr>" +
+                  "<td>" + name + "</td>" +
+                  "<td>" + description + "</td>" +
+                  "<td>" + category + "</td>" +
+                  "<td> <input type='radio' name='import' class='form-check-input' value='"+id+"'></td>" +
+              "</tr>";              
+              $("#tbody").append(tr_str);
+              i++;
+            }          
+          }else{
+             var tr_str = "<tr>" +
+                 "<td align='center' colspan='4'>No record found.</td>" +
+             "</tr>";
+
+             $("#table tbody").append(tr_str);
+          }
+
+        }
+      });
+    }
+    
+    function fetchQuestions(){
+      var module = $("input[name='import']:checked").val(); 
+      if(module != null){
+      $.ajax({
+        url: "{{route('admin.modules.getquestions')}}",
+        headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+        type: 'POST',
+        data: {module: module},
+        dataType: 'json',
+        success: function(response){ 
+        $("#tbodyquestions").empty();
+          var len = 0;          
+          if(response[0].name != null){
+            len = 1;
+          }         
+          if(len > 0){
+            i = 0;
+            while(response[i].name != null)
+            {
+              var label_left = response[i].label_left;
+              var name = response[i].name; 
+              var label_right = response[i].label_right;
+              var max_rate = response[i].max_rate;
+              var id = response[i].id;
+
+              var tr_str = "<tr>" +
+                  "<td>" + name + "</td>" +
+                  "<td>" + label_left + "</td>" +
+                  "<td>" + label_right + "</td>" +
+                  "<td>" + max_rate + "</td>" +
+                  "<td> <input type='checkbox' name='importQuestions' class='form-check-input' value='"+id+"'></td>" +
+              "</tr>";              
+              $("#tbodyquestions").append(tr_str);
+              i++;
+            }          
+          }else{
+             var tr_str = "<tr>" +
+                 "<td align='center' colspan='4'>No record found.</td>" +
+             "</tr>";
+
+             $("#table tbody").append(tr_str);
+          }
+          
+         // alert("Questions imported");
+        }
+      });
+      }
+      else{
+      alert("Select a module");
+      }
+    }
+    
+    
+    $('#insertQuestions').click(function(){
+        
+        importQuestions();
+       
+       });
+    
+    
+    function importQuestions(){
+      var questions = [];
+            $.each($("input[name='importQuestions']:checked"), function(){            
+                questions.push($(this).val());
+            });
+      if(questions != null){
+      $.ajax({
+        url: "{{route('admin.modules.importquestions')}}",
+        headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+        type: 'POST',
+        data: {questions: questions},
+        dataType: 'json',
+        success: function(response){ 
+                var i = 0;
+                while(response[i].name != null)
+                {
+                    var code = '<br>\n\
+        <div class="card" id="'+response[i].id+'">\n\
+            <h5 class="card-title"> Insert question </h5>\n\
+            <div class="item-content card-body block">\n\
+                <div class="form-group">\n\
+                    <label  class="col-lg-2 control-label">Question</label>\n\
+                    <div class="col-lg-10">\n\
+                        <input type="text" class="form-control question form-control-lg" value="'+response[i].name+'">\n\
+                    </div>\n\
+                </div>\n\
+            <div class="row">\n\
+                <div class="col">\n\
+                    \n\
+                    <input type="text" class="form-control leftlabel" value="'+response[i].label_left+'" ></div>\n\
+                <div class="col"><input type="text" class="form-control rightlabel" value="'+response[i].label_right+'" ></div>\n\
+            </div>\n\
+            <div class="row">\n\
+                    <div class="col">\n\
+                        <div class="form-group">\n\
+                            <label class="control-label ">Max mark value</label>\n\
+                            <input type="text" class="form-control maxmark" value="'+response[i].max_rate+'" >\n\
+                        </div>\n\
+                    </div>\n\
+                    <div class="col">\n\
+                        <div class="form-group">\n\
+                            <label class="control-label ">Correct answer</label>\n\
+                            <input type="text" class="form-control correctans" value="'+response[i].correct_answer+'"></div></div></div></div><div class="pull-right">\n\
+                <button class="btn btn-danger remove-btn" onclick="rimuovi('+response[i].id+')">Remove</button>\n\
+            </div>\n\
+            <div class="clearfix"></div>\n\
+        </div><br>';
+                    var code = '<br><div class="card border-primary mb-3" id="'+response[i].id+'"><div class="item-content card-body block align-self-lg-auto"><div class="form-group"><h5 for="question" class="col-lg-5 control-label">Question</h5><div class="col-lg-12 col-md-9 col-sm-5"><input type="text" class="form-control question"   value="'+response[i].name+'" ></div></div><div class="row"><div class="col"><h5 for="leftlabel" class="col-lg-6 control-label">Left label</h5><input type="text" id="leftlabel" name="leftlabel" class="form-control leftlabel" value="'+response[i].label_left+'"></div><div class="col"><h5 for="rightlabel"  class="col-lg-6 control-label">Right label</h5><input type="text" id="rightlabel" name="rightlabel" class="form-control rightlabel" value="'+response[i].label_right+'"><br></div></div><div class="row"><div class="col"><div class="form-group"><h5 for="maxmark" class="control-label ">Max value</h5><input type="text" class="form-control maxmark" id="maxmark"  value="'+response[i].max_rate+'"></div></div><div class="col"><div class="form-group"><h5 for="correctans" class="control-label ">Correct answer</h5><input type="text" class="form-control correctans" id="correctans"  value="'+response[i].correct_answer+'"></div></div></div></div><div class="pull-right repeater-remove-btn"><button class="btn btn-danger remove-btn" onclick="rimuovi('+response[i].id+')">Remove question</button></div><div class="clearfix"></div></div>';
+                    $("#questions").append(code);
+                    i++;
+                }
+          }
+          
+         // alert("Questions imported");
+        });
+      }
+      }  
+      
+    });
+    function rimuovi(elemID){
+       var element = document.getElementById(elemID);
+       element.parentNode.removeChild(element);
+    }
+    </script>
 @endsection
